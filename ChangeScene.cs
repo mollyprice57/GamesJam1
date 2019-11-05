12abf825c86307dac2ba111048cb4f2c4ea8b0d1
molly.price57 <43675124+mollyprice57@users.noreply.github.com>
2019-11-05 10:15:24 +0000

    Basic Code
    This is the changing scene code and reset level code after hitting a hazard

---
+++ ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+ {
+    public BoxCollider2D endGoal;
+    public BoxCollider2D player;
+    public string level;
+
+
+    void Update()
+    {
+        if (player.IsTouching(endGoal))
+        {
+            ResetLevel();
+        }
+    }
+
+    void ResetLevel()
+    {
+        Application.LoadLevel(level);
+    }
+}
---
+++ DeathTouch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTouch : MonoBehaviour
+{
+    public BoxCollider2D hazard;
+    public BoxCollider2D player;
+    public string level;
+
+
+    void Update()
+    {
+        if (hazard.IsTouching(player))
+        {
+            ResetLevel();
+        }
+    }
+       
+    void ResetLevel()
+    {
+        Application.LoadLevel(level);
+    }
+}
